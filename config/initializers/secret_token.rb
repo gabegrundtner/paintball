d3cc2ee8d36082146e5a3e118e751ba529ec4bf0
Gabe Grundtner <gbgrun10@smumn.edu>
2013-03-18 17:42:19 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paintball::Application.config.secret_token = 'ebc9be2f951cc7f2d5b3c7e4f9d0082c55f0dc084ebece6320c54fe697754006d377944f927052e7f25e0374dc798ef9baa3b036dd79c6bc61da26ed2ecee0b5'
