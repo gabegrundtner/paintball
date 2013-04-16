a2c4a740a90760f10931a2ba8364fe503ef6927c
Gabe Grundtner <gbgrun10@smumn.edu>
2013-04-16 09:03:41 -0500

    quick fixed a test for user stories 10, 1 and 12

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 describe "Static pages"  do
 	describe "Home page" do
 		#user story 10
-		it "should have content 'home of SMU Kids'" do
+		it "should have content 'home of SMU Kids' and links to tournaments and players" do
 			visit '/static_pages/home'
 			page.should have_content('HOME OF THE SMU KIDS')
+			page.should have_link("tournaments", href: tournaments_path)
+			page.should have_link("players", href: users_path)
 		end
-		#it { should have_link("tournaments", href: tournaments_path) }
-		#it { should have_link("players", href: users_path) }
 	end
 	
 end
