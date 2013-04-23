9121c83ed47ddf8d676cb98b7c75af2a12f1e62c
Gabe Grundtner <gbgrun10@smumn.edu>
2013-04-23 02:12:23 -0500

    updated seeds file to fit data type updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,14 @@ u1 = User.create!(email: "gbgrun10@smumn.edu", is_admin: true, name: "Gabe", pas
 u2 = User.create!(email: "saric10@smumn.edu", is_admin: true, name: "Sam", password: "password", status: "junior")
 u3 = User.create!(email: "jfbean10@smumn.edu", is_admin: true, name: "Jim", password: "password", status: "junior")
 
-t1 = Tournament.create!(date: "12/12/12", location: "River Falls, WI")
-t2 = Tournament.create!(date: "4/13/13", location: "Minnetonka, MN")
-t3 = Tournament.create!(date: "9/4/13", location: "Apple Valley, MN")
+t1 = Tournament.create!(date: "2012/12/12", location: "River Falls, WI")
+t2 = Tournament.create!(date: "2013/4/13", location: "Minnetonka, MN")
+t3 = Tournament.create!(date: "2008/4/13", location: "Apple Valley, MN")
 
-p1 = Participant.create!(tournament_id: t1, user_id: u1)
-p2 = Participant.create!(tournament_id: t2, user_id: u1)
-p3 = Participant.create!(tournament_id: t3, user_id: u1)
+p1 = Participant.create!(tournament_id: t1.id, user_id: u1.id)
+p2 = Participant.create!(tournament_id: t2.id, user_id: u1.id)
+p3 = Participant.create!(tournament_id: t3.id, user_id: u1.id)
 
-r1 = Result.create!(tournament_id: t1, place: "1st", team_name: "Saint Mary's")
-r2 = Result.create!(tournament_id: t2, place: "1st", team_name: "Saint Mary's")
-r3 = Result.create!(tournament_id: t3, place: "1st", team_name: "Saint Mary's")
+r1 = Result.create!(tournament_id: t1.id, place: "1st", team_name: "Saint Mary's")
+r2 = Result.create!(tournament_id: t2.id, place: "1st", team_name: "Saint Mary's")
+r3 = Result.create!(tournament_id: t3.id, place: "1st", team_name: "Saint Mary's")
